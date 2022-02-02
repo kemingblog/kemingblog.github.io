3eec24bfbf3890a2fb21cc1522096a3258fded4e
ThemeSchemer <94884864+themeschemer@users.noreply.github.com>
2022-02-02 11:58:02 -0600

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+  document.getElementById('logo-fade').style.opacity = '1';
+  document.getElementById('logo-fade').style.marginLeft = '50px';
+  document.getElementById('logo-spacing').style.opacity = '1';
+}, 0);
+
+setTimeout(function() {
+  document.getElementById('logo-spacing').style.letterSpacing = '-4px';
+}, 1500);
