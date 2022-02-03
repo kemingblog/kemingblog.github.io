e329b331adca00f4a457dfbb4ff27cf2d01706b3
ThemeSchemer <94884864+themeschemer@users.noreply.github.com>
2022-02-02 21:53:27 -0600

    Add files via upload

---
+++ monthlyfont.js
@@ -0,0 +1,33 @@
+setInterval(function() {
+  document.getElementById("monthly-1").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-3").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-5").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-7").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-9").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-11").style.marginLeft = (window.scrollY - 1500) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-2").style.marginLeft = "-" + (window.scrollY - 800) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-4").style.marginLeft = "-" + (window.scrollY - 800) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-6").style.marginLeft = "-" + (window.scrollY - 800) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-8").style.marginLeft = "-" + (window.scrollY - 800) + "px" ;
+}, 0)
+setInterval(function() {
+  document.getElementById("monthly-10").style.marginLeft = "-" + (window.scrollY - 800) + "px" ;
+}, 0)
